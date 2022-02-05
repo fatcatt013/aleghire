18dcbba3b6284220b8b5cfbff3a2c752ffac9f3b
mallasorte12 <mallasorte12@gmail.com>
2022-02-05 17:37:07 +0100

    navigation-v1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,34 @@
 import React from 'react';
-
+import { GiTreasureMap, GiSwordClash } from 'react-icons/gi';
+import { FaSitemap, FaDungeon, FaCross } from 'react-icons/fa';
+import { MdLocationCity, MdGroups, MdOutlineMail, MdSettings } from 'react-icons/md';
+import { GrGroup } from 'react-icons/gr';
 const Navbar = () => {
 
     return (
-        <div className='navigation-container'>
-            <ul>
+        <div className='nav-container'>
+            <ul className='nav-ul'>
                 <li>
                     <div className='nav-profile'>
-                        <img src="https://i.pinimg.com/564x/c6/fc/2c/c6fc2c165498a2f39b420b1e75aa2017.jpg"
+                        <img src="https://i.pinimg.com/564x/0e/e6/05/0ee6058a98ee88e7c59f64ea4d9592a8.jpg"
                              alt="" className='mini-pfp'/>
-                        <span>Abbon</span><br/>
-                        <span>1000g</span>
+                             <div>
+                                 <span>Abbon</span><br/>
+                                 <span>1000g</span>
+                             </div>
                     </div>
                 </li>
-                <hr/>
-                <li>Map</li>
-                <li>City</li>
-                <li>Religion</li>
-                <li>Dungeon</li>
-                <li>Guild</li>
-                <li></li>
+                <div className='nav-items'>
+                    <li><GiTreasureMap/> Map</li>
+                    <li><FaSitemap/> Talent Tree</li>
+                    <li><MdLocationCity/> City</li>
+                    <li><GiSwordClash/> Arena</li>
+                    <li><FaDungeon/> Dungeon</li>
+                    <li><MdGroups/> Guild</li>
+                    <li><FaCross/> Religion</li>
+                    <li><MdOutlineMail/> Mailbox</li>
+                    <li><MdSettings/> Settings</li>
+                </div>
             </ul>
         </div>
     )
--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -7,7 +7,7 @@ const ProfileInfo = () => {
     return (
         <div className='profile-info'>
             <h2>Abbon</h2>
-            <img src="https://i.pinimg.com/564x/c6/fc/2c/c6fc2c165498a2f39b420b1e75aa2017.jpg" alt="" className='profile-info-image'/>
+            <img src="https://i.pinimg.com/564x/0e/e6/05/0ee6058a98ee88e7c59f64ea4d9592a8.jpg" alt="" className='profile-info-image'/>
             <div className='profile-info-paragraphs'>
                 <p>Level: 1</p>
                 <p>Experience: 0/100</p>
